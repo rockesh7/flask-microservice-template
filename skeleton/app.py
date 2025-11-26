4ed7772382c8956d29da2df3f6b442198bc2c2d8
Rockesh Sam Manovah <rockesh.sam.manovah07@gmail.com>
2025-11-26 11:08:00 +0530

    Update app.py

--- skeleton/app.py
+++ skeleton/app.py
@@ -15,4 +15,4 @@ def metrics():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000)
-    app.run(host='0.0.0.0', port=${{ port }})
+    app.run(host='0.0.0.0', port=${{ values.port }})
